5e89f75ce9150d47478483978a0af896881116a8
hadicodess <85850834+hadicodess@users.noreply.github.com>
2021-07-29 22:10:12 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ function take_snapshot()
 console.log('ml5 version', ml5.version); 
 
 classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JYfo80ezB/',modelLoaded); 
+ 
+function modelLoaded() {
+    console.log("Model Loaded")
+} 
+
+function check() 
+{
+    img = document.getElementById('captured_image') 
+    classifier.classify(img, gotResult); 
+} 
+gotResult(error, results) 
+{
+    if(error){
+        console.error(error); 
+    } 
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label; 
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3); 
+    }
+}
